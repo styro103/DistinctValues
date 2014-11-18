9f9a7dd0ae2440b28699d573485cf253851c7a6d
styro103 <smbateng103@yahoo.com>
2014-11-18 14:52:47 -0500

    Added/Updated Comments
    Added to and updated comments of main file that calls classes.

--- src/Distinct.java
+++ src/Distinct.java
@@ -1,3 +1,11 @@
+/*
+ * Shaun Mbateng
+ * This program has the user input an array, and then displays the number
+ * 	of distinct elements in the array.
+ * It has a time complexity of O(N*log(N))
+ * 	(complexity is O(N) if sorting method isn't used).
+ */
+
 import java.util.Scanner;
 
 public class Distinct 
@@ -25,10 +33,15 @@ public class Distinct
 			A[i] = cin.nextInt();
 		}
 		
-		count1 = Set1.getCount(A); //Get Count Using Prefix Sums
+		count1 = Set1.getCount(A); //Get Count Using Array of Counters
 		count2 = Set2.getCount(A); //Get Count Using Sorting
 		
 		if (count1==count2) //If Equal, and Always Should be, Display
 			System.out.println("Number of Distinct Elements: "+count1);
+		else //Should Not Happen, but for Code Checking
+		{
+			System.out.println("Number of Distinct Elements:  1: "+count1);
+			System.out.println("Number of Distinct Elements:  2: "+count2);
+		}
 	}
 }
